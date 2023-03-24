dc83e93a9d1b37902fc63493381508faa62541fa
JackVM42 <124638941+JackVM42@users.noreply.github.com>
2023-03-24 15:15:24 -0400

    Added methods in order to sort in reverse using same algorithms

--- src/main/java/com/mycompany/quicksortproject2/QuickSortProject2.java
+++ src/main/java/com/mycompany/quicksortproject2/QuickSortProject2.java
@@ -19,7 +19,7 @@ public class QuickSortProject2 {
     public static void main(String[] args) {
         
         //make an array of certain length with certain max number
-        int[] arr = makeArray(11, 10);
+        int[] arr = makeArray(10, 10);
         
         
         //print before staement with order of array items using printArray method
@@ -30,7 +30,16 @@ public class QuickSortProject2 {
         quicksort(arr);
         
         //print out after statement
-        System.out.println("After: ");
+        System.out.println("After Accending: ");
+        printArray(arr);
+        
+        //For reverse
+        
+        //use quicksort method on our array
+        revQuicksort(arr);
+        
+        //print out after statement
+        System.out.println("After Reverse: ");
         printArray(arr);
     }
     
@@ -42,17 +51,17 @@ public class QuickSortProject2 {
      * @param arr the array which is to be sorted
      */
     private static void quicksort(int[] arr) {
-    quicksort(arr, 0, arr.length - 1);
+    quicksort(arr, 0, arr.length - 1, 1);
   }
     
     /**
-     * Method which uses the quicksort algorithm in order to sort an array of ints in accending order
+     * Method which uses the quicksort algorithm in order to sort an array of ints in order based off order
      * @param arr Array to be sorted
      * @param lowInd Index of the first item in the array to be sorted
      * @param highInd Index of the last item to be sorted
+     * @param order 1 means accending, 0 means decending
      */
-    private static void quicksort(int[] arr, int lowInd, int highInd) {
-        
+    private static void quicksort(int[] arr, int lowInd, int highInd, int order) {
         
         //makes sure that if your algorithm is tyrying to sort a single number it will just quit here since it is already in order
     if (lowInd >= highInd) {
@@ -60,26 +69,34 @@ public class QuickSortProject2 {
     }
 
     int pivot = medianPivot(arr, lowInd, highInd);
-
+    
     //partions our array section using the partition method and then gets a return of the left pointer variable at the end of teh partition method
-    int lp = partition(arr, lowInd, highInd, pivot);
+    //For accending order
+    int lp = 0;
+    
+    if(order == 1){
+        lp = partition(arr, lowInd, highInd, pivot);
+    }
+    
+    //for decending
+    if(order == 0){
+        lp = revPartition(arr, lowInd, highInd, pivot);
+    }
 
-    
-    
-    
+
     //recursively quicksorts our smaller and smaller sections until the sections become of length one and the return statement at the top runs
     if(lp - 1 - lowInd > 10){
-        quicksort(arr, lowInd, lp - 1);
+        quicksort(arr, lowInd, lp - 1, order);
     }
     else{
-        insertionSort(arr, lowInd, lp - 1);
+        insertionSort(arr, lowInd, lp - 1, order);
     }
     
     if(highInd - lp + 1 > 10){
-       quicksort(arr, lp + 1, highInd); 
+       quicksort(arr, lp + 1, highInd, order); 
     }
     else{
-       insertionSort(arr, lp + 1, highInd);
+       insertionSort(arr, lp + 1, highInd, order);
     }
 
   }
@@ -248,7 +265,7 @@ public class QuickSortProject2 {
      * picks median of teh first last and mid value in the array and uses that as the pivot
      * @param arr array to chose pivot in
      * @param lowInd low index we are working with
-     * @param highInd the high index of teh array section we are working on
+     * @param highInd the high index of the array section we are working on
      * @return the value of the pivot
      */
     private static int medianPivot(int[] arr, int lowInd, int highInd){
@@ -264,10 +281,15 @@ public class QuickSortProject2 {
      * @param arr array to be sorted
      * @param lowInd low index of section of array being sorted
      * @param highInd high index of part of array being sorted
+     * @param order Accending or decending
      */
-    private static void insertionSort(int[] arr, int lowInd, int highInd){
-
+    private static void insertionSort(int[] arr, int lowInd, int highInd, int order){
+        
+        //if order is accending
+        if(order == 1){
+            //iterates through each item in the array
 	    for (int i = lowInd + 1; i <= highInd; i++) {
+                //this one iterates back through the array one element at a time
 		    for(int j = i ; j > lowInd ; j--){
 			    
                             if(arr[j] < arr[j-1]){
@@ -275,7 +297,71 @@ public class QuickSortProject2 {
 			    }
 		    }
 	    }
+        }
+        
+        //if order is decending
+        if(order == 0){
+            //iterates through each item in the array
+	    for (int i = lowInd + 1; i <= highInd; i++) {
+                //this one iterates back through the array one element at a time
+		    for(int j = i ; j > lowInd ; j--){
+			    
+                            if(arr[j] > arr[j-1]){
+				    swap(arr, j, j-1);
+			    }
+		    }
+	    }
+        }
+
+            
     }
+    
+    
+    /**
+     * Sorts an array of ints in decending order
+     * Overloaded method which allows you to quicksort an int array just passing an array without needing and upper and lower index
+     * @param arr the array which is to be sorted
+     */
+    private static void revQuicksort(int[] arr) {
+    quicksort(arr, 0, arr.length - 1, 0);
+  }
+    
+    
+    
+    private static int revPartition(int[] arr, int lowInd, int highInd, int pivot) {
+    
+       //left pointer starts at the index of the bottom if the partition section and right pointer starts one left of the pivot number
+    int lp = lowInd;
+    int rp = highInd - 1;
+
+    //stops running once the two pointers have reached eachother
+    while (lp < rp) {
+
+      // lp works right through the array until it finds a number greater than the pivot number, or reaches the right pointer.
+      while (arr[lp] >= pivot && lp < rp) {
+        lp++;
+      }
+
+      // rp works left through the array until it finds a number less than the pivot number, or reaches the left pointer.
+      while (arr[rp] <= pivot && lp < rp) {
+        rp--;
+      }
+        
+      //once the left pointer has a number bigger than the pivot and the right pointer has a number smaller we can just switch them
+      swap(arr, lp, rp);
+    }
+    
+    //makes sure that the last number is in order
+    if(arr[lp] < arr[highInd]) {
+      swap(arr, lp, highInd);
+    }
+    else {
+      lp = highInd;
+    }
+    
+    //returns the value of left pointer
+    return lp;
+  }
     
 }
 
